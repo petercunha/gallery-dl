ae8862a30576ec9e34a9ac307740edc59ba97859
Peter Cunha <petercunha8@gmail.com>
2026-02-09 00:22:17 -0800

    Update showcamrips.py

--- gallery_dl/extractor/showcamrips.py
+++ gallery_dl/extractor/showcamrips.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 
-# Copyright 2026 Mike Fährmann
-#
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License version 2 as
 # published by the Free Software Foundation.
